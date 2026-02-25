ddca44de5cad04d1116b613a25cc4b81fc9c6685
Khairibyan <byanpts@gmail@gamil.com>
2026-02-25 12:18:59 +0700

    Percobaan 3

---
+++ MahasiswaMain15.java
@@ -0,0 +1,54 @@
+public class MahasiswaMain15 {
+
+    static class Mahasiswa {
+        String nama;
+        String nim;
+        String kelas;
+        double ipk;
+
+        void tampilkanInformasi() {
+            System.out.println("Nama: " + nama);
+            System.out.println("NIM: " + nim);
+            System.out.println("Kelas: " + kelas);
+            System.out.println("IPK: " + ipk);
+        }
+
+        void ubahKelas(String kelasBaru) {
+            kelas = kelasBaru;
+        }
+
+        void updateIpk(double ipkBaru) {
+            ipk = ipkBaru;
+        }
+
+        Mahasiswa() {
+        }
+
+        Mahasiswa(String nama, String nim, double ipk, String kelas) {
+            this.nama = nama;
+            this.nim = nim;
+            this.ipk = ipk;
+            this.kelas = kelas;
+        }
+    }
+
+    public static void main(String[] args) {
+        Mahasiswa mhs1 = new Mahasiswa();
+        mhs1.nama = "Muhammad Ali Farhan";
+        mhs1.nim = "2241720171";
+        mhs1.kelas = "SI 2J";
+        mhs1.ipk = 3.55;
+        
+        mhs1.tampilkanInformasi();
+        mhs1.ubahKelas("SI 2K");
+        mhs1.ubahKelas(mhs1.kelas);
+        mhs1.updateIpk(3.60);
+        mhs1.updateIpk(mhs1.ipk);
+        mhs1.tampilkanInformasi();
+
+        Mahasiswa mhs2 = new Mahasiswa("Anis Fahrotun nisa","254107060124", 3.25,"TI 1F");
+        mhs2.updateIpk(3.30);
+        mhs2.tampilkanInformasi();
+
+    }
+}
